5cb709e998df9a02fb9be68a36c997174142153a
RegishShrestha <regishshrestha@gmail.com>
2022-09-09 22:35:35 +0545

    second commit

--- script.js
+++ script.js
@@ -3,6 +3,36 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
+// ///////////////////////////////////////
 
-const request = new XMLDocument();
+const genCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('Get', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const curr = data.currencies['name'];
+    console.log(curr);
+    const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.official}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${data.languages}</p>
+                <p class="country__row"><span>💰</span>${data.currencies}</p>
+            </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+genCountryData('portugal');
+genCountryData('usa');
+
+// https://restcountries.com/v2/
