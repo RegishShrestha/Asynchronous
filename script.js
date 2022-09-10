ef8d19f035da0b9e8180ef6cac3160286c96771a
RegishShrestha <regishshrestha@gmail.com>
2022-09-10 12:06:37 +0545

    databynneighbour added

--- script.js
+++ script.js
@@ -5,28 +5,65 @@ const countriesContainer = document.querySelector('.countries');
 
 // ///////////////////////////////////////
 
-const genCountryData = function (country) {
+// const genCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('Get', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     console.log(Object.values(data.currencies)[0].name);
+//     const html = `
+//         <article class="country">
+//             <img class="country__img" src="${data.flags.png}" />
+//             <div class="country__data">
+//                 <h3 class="country__name">${data.name.official}</h3>
+//                 <h4 class="country__region">${data.region}</h4>
+//                 <p class="country__row"><span>👫</span>${(
+//                   +data.population / 1000000
+//                 ).toFixed(1)} people</p>
+//                 <p class="country__row"><span>🗣️</span>${
+//                   Object.values(data.languages)[0]
+//                 }</p>
+//                 <p class="country__row"><span>💰</span>${
+//                   Object.values(data.currencies)[0].name
+//                 }</p>
+//             </div>
+//         </article>`;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// genCountryData('portugal');
+// genCountryData('usa');
+// genCountryData('germany');
+
+const genCountryDataAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('Get', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    const curr = data.currencies['name'];
-    console.log(curr);
+    console.log(Object.values(data.currencies)[0].name);
     const html = `
-        <article class="country">
-            <img class="country__img" src="${data.flags.png}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name.official}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(
-                  +data.population / 1000000
-                ).toFixed(1)} people</p>
-                <p class="country__row"><span>🗣️</span>${data.languages}</p>
-                <p class="country__row"><span>💰</span>${data.currencies}</p>
-            </div>
-        </article>`;
+          <article class="country">
+              <img class="country__img" src="${data.flags.png}" />
+              <div class="country__data">
+                  <h3 class="country__name">${data.name.official}</h3>
+                  <h4 class="country__region">${data.region}</h4>
+                  <p class="country__row"><span>👫</span>${(
+                    +data.population / 1000000
+                  ).toFixed(1)} people</p>
+                  <p class="country__row"><span>🗣️</span>${
+                    Object.values(data.languages)[0]
+                  }</p>
+                  <p class="country__row"><span>💰</span>${
+                    Object.values(data.currencies)[0].name
+                  }</p>
+              </div>
+          </article>`;
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
   });
@@ -34,5 +71,6 @@ const genCountryData = function (country) {
 
 genCountryData('portugal');
 genCountryData('usa');
+genCountryData('germany');
 
 // https://restcountries.com/v2/
